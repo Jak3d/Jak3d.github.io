05e665b827bc6b8cf0ebc7f07a23b9c8004bff00
Jak3d <marroleonardo@gmail.com>
2022-09-29 15:01:56 +0200

    Aggiunto SOLAB

--- src/components/ElementiDiPRobStat.vue
+++ src/components/ElementiDiPRobStat.vue
@@ -1,11 +1,13 @@
 <template>
-    
-<h2 class="subtitle is-3">Elementi di Probabilità e Statistica</h2>
+    <div class="content is-normal">
+        <h2 class="subtitle is-2">Elementi di Probabilità e Statistica</h2>
+        <h2 class="subtitle is-3">Lez 4</h2>
 
+    </div>
 </template>
 <script>
-    export default{
-        name:'ElemProbStat',
+export default {
+    name: 'ElemProbStat',
 
-    }
+}
 </script>
